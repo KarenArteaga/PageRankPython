61ed2f8c2f5243c629fc3fa998c2781e9fb6ab93
KarenArteaga <69361149+KarenArteaga@users.noreply.github.com>
2022-05-18 20:15:55 -0500

    Update page_rank.py

--- page_rank.py
+++ page_rank.py
@@ -8,14 +8,29 @@ Created on Tue May 17 11:15:32 2022
 import networkx as nx
 import matplotlib.pyplot as plt
 import random as rd
+import pandas as pd
 
 def dic_comprobacion(grafo):
     # Usar el Page Rank de networkx
     lista = nx.pagerank(grafo)
 
     # ordenar los nodos
-    paginas_ordenadas = sorted(lista.items(),key = lambda v: (v[1], v[0]), reverse = True)
+    paginas_ordenadas = sorted(lista.items(),key = lambda v: [v[1], v[0]], reverse = True)
     return paginas_ordenadas
+
+# generar un dataframe para los resultados
+def resultados(mi_ranking, netw_ranking):
+    # agregar datos obtenidos
+    df1 = pd.DataFrame(mi_ranking, columns = ['     Nuestro', 'ranking    '])
+    # agregar datos de Networkx
+    df2 = pd.DataFrame(netw_ranking)
+    df1['     NetworkX'] = df2[0]
+    df1['ranking     '] = df2[1]
+    df1.loc[-1] = ['Nodo', 'Valores', 'Nodo', 'Valores']
+    df1.index = df1.index + 1  
+    df1.sort_index(inplace = True) 
+    df1.style
+    print(df1)
 
 
 def page_rank(num_nodos, prob):
@@ -43,24 +58,28 @@ def page_rank(num_nodos, prob):
       else:
         x = rd.choice(list_n)
         ranking_dic[x] = ranking_dic[x] + 1
-      
-    print("Se actualizó el puntaje")
+     
     
     # Normalizar valores
     for j in range(0, num_nodos):
       ranking_dic[j] = ranking_dic[j]/600000
     
     
-    # Ordenar los nodos
-    mi_ranking = sorted(ranking_dic.items(), key = lambda v: (v[1], v[0]), reverse = True)
+    # Ordenar los nodos por puntaje
+    mi_ranking = sorted(ranking_dic.items(), key = lambda v: [v[1], v[0]], reverse = True)
+    
+    # generar ranking de Networkx
     netw_ranking = dic_comprobacion(grafo)
     
-    # Comparar los resultados
-    print("\nMi Algoritmo ---- NetworkX\n")
-    for i in range(num_nodos):
-      print((i+1), ".", '{:>8}  {:>8}'.format(mi_ranking[i][0], netw_ranking[i][0]))
+    # mostrar resultados
+    resultados(mi_ranking, netw_ranking)
+    
 
-
+# resultados para grafo con 10 nodos
 page_rank(10, 0.6)
-page_rank(25, 0.6)
-page_rank(50, 0.6)
+
+# resultados para grafo con 25 nodos
+#page_rank(25, 0.6)
+
+# resultados para grafo con 50 nodos
+#page_rank(50, 0.6)
